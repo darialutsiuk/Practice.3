da30247c9bfc038ec68cf20071e95f308530e2e6
darialutsiuk <lutsiukdaria16@gmail.com>
2026-05-05 02:34:05 +0300

    added program

--- Practice.3/Program.cs
+++ Practice.3/Program.cs
@@ -1,6 +1,19 @@
-namespace Practice._3;
+using Practice._3;
 
-public class Program
-{
-    
-}
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+BankCard card1 = new BankCard("1234-5678", "Дарія", 5000);
+Console.WriteLine(card1);
+
+BankCard card2 = new BankCard("1111-2222", "Дмитро");
+Console.WriteLine(card2);
+
+Console.WriteLine($"Кількість карток: {BankCard.GetCount()}");
+
+card1.SetLimit(8000);
+Console.WriteLine($"Новий ліміт: {card1.Limit}");
+
+card1.SetLimit(-100);
+
+Console.WriteLine($"Операція 3000 грн дозволена? {card1.IsOperationAllowed(3000)}");
+Console.WriteLine($"Операція 10000 грн дозволена? {card1.IsOperationAllowed(10000)}");
